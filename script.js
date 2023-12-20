e89d0514a2ee95bb5db013835e41149201b98f41
Aniket chaturvedi <aniketchaturvedi309@gmail.com>
2023-12-20 18:44:21 +0530

    add reset buton

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 // console.log("hello I am working ");
 let computerVal;
-let score=0;
-let mxscore=0;
+// if "JSON.parse(localStorage.getItem(count)) " this statement is NULL in below then it set by default
+let count=null;
 
+document.querySelector('.reset').addEventListener('click',function(){
+    localStorage.removeItem('count');
+    count={
+        score : 0,
+        mxscore : 0
+    }
+    console.log('hahksd');
+    displayScore();
+})
+
+count=JSON.parse(localStorage.getItem('count')) || {
+    score : 0,
+    mxscore : 0
+};
+
+ // To display score
+let displayScore = function(){
+
+    let Score=document.querySelector('.score');
+    Score.innerText=`SCORE = ${count.score}`
+    let maxScore=document.querySelector('.maxScore');
+    maxScore.innerText = `MAXSCORE = ${count.mxscore}`
+}
+
+// This function is used to find the computer move
 let find = function(computerVal){
     if(computerVal >= 0 && computerVal <= (1/3)){
         return 'ROCK';
@@ -15,25 +40,24 @@ let find = function(computerVal){
     }
 }
 
-let scorefun = function(){
-    let Score=document.querySelector('.score');
-    Score.innerText=`SCORE = ${score}`;
-}
 
-let maxscorefun=function(){
-    let maxScore=document.querySelector('.maxScore');
-    maxScore.innerText = `MAXSCORE = ${mxscore}`
-}
+let Score=document.querySelector('.score');
+Score.innerText=`SCORE = ${count.score}`
 
-setInterval(scorefun,1);
-setInterval(maxscorefun,1);
+// To display score
+let maxScore=document.querySelector('.maxScore');
+maxScore.innerText = `MAXSCORE = ${count.mxscore}`
 
 let box = document.querySelectorAll('.box');
 Array.from(box).forEach(function(element){
+    
     let boxText=element.querySelector('.boxText');
     let text=document.querySelector('.text');
     let computer = document.querySelector('.computer');
+
+    // after click on box like "rock || paper || scissor"
     element.addEventListener('click' , ()=>{
+
         computerVal = Math.random();
         let computerSel = find(computerVal);
         let yoursel=boxText.innerText;
@@ -42,43 +66,46 @@ Array.from(box).forEach(function(element){
 
         if(yoursel === computerSel){
             text.innerText='TIE';
-            
         }
         else if(yoursel === 'PAPER' && computerSel === 'ROCK'){
             text.innerText='YOU WIN';
-            score++;
-            mxscore=Math.max(score,mxscore);
+            count.score++;
+            mxscore=Math.max(count.score,count.mxscore);
             
         }
         else if(yoursel === 'PAPER' && computerSel === 'SCISSOR'){
             text.innerText='YOU LOSS';
-            score--;
-            mxscore=Math.max(score,mxscore);
+            count.score--;
+            count.mxscore=Math.max(count.score,count.mxscore);
 
         }
         else if(yoursel === 'ROCK' && computerSel === 'PAPER'){
             text.innerText='YOU LOSS';
-            score--;
-            mxscore=Math.max(score,mxscore);
+            count.score--;
+            count.mxscore=Math.max(count.score,count.mxscore);
 
         }
         else if(yoursel === 'ROCK' && computerSel === 'SCISSOR'){
             text.innerText='YOU WIN';
-            score++;
-            mxscore=Math.max(score,mxscore);
+            count.score++;
+            count.mxscore=Math.max(count.score,count.mxscore);
 
         }
         else if(yoursel === 'SCISSOR' && computerSel === 'PAPER'){
             text.innerText='YOU WIN';
-            score++;
-            mxscore=Math.max(score,mxscore);
+            count.score++;
+            count.mxscore=Math.max(count.score,count.mxscore);
 
         }
         else if(yoursel === 'SCISSOR' && computerSel === 'ROCK'){
             text.innerText='YOU LOSS';
-            score--;
-            mxscore=Math.max(score,mxscore);
+            count.score--;
+            count.mxscore=Math.max(count.score,count.mxscore);
 
         }
+        
+        // To save all score into a local storage
+        localStorage.setItem("count" , JSON.stringify(count));
+        displayScore();
     })
 })
